1b97fe19ce1d2e18a03ba07d20ff9f0ff1eecf82
mikheil sapharidze <portofranko36@gmail.com>
2021-08-30 11:18:49 +0200

    done

---
+++ src/Button/Button.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+class Button extends Component {
+  static propTypes = { fn: propTypes.func };
+
+  render() {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+    return (
+      <button
+        className="Button"
+        type="button"
+        onClick={(event) => this.props.fn()}
+      >
+        Load more
+      </button>
+    );
+  }
+}
+
+export default Button;
